41d2bba641926fe68e9c2b96530b3b84cb7b9d49
Samuel Paul Christopher <thesamchris@gmail.com>
2019-09-12 17:06:41 +0800

    initial commit

---
+++ packing.c
@@ -0,0 +1,27 @@
+/**
+ * CS1010 AY2017/8 Semester 1 Lab1 Ex3
+ * packing.c
+ * This program reads in the dimensions of a tray and a slab, and computes the maximum possible number of slabs that could be packed onto the tray.
+ * Samuel Paul Christopher
+ * C08
+ */
+
+#include <stdio.h>
+
+// Write your function prototype below (and remove this comment!)
+
+int main(void) {
+	int max_slabs;
+
+	printf("Enter dimension of tray: ");
+
+	printf("Enter dimension of slab: ");
+
+	printf("Maximum number of slabs = %d\n", max_slabs);
+
+	return 0;
+}
+
+// Add your function below (and remove this comment!)
+// Every function should be preceded with a comment
+// that describes what the function does.
